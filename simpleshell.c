59bc4b69b77f9f8c77b2dd0999717c1faa2e6246
vaibhav <vaibhav.kurhe@gmail.com>
2016-01-24 10:40:37 +0530

    Added strtok() to parse the input string into command and its arguments

--- simpleshell.c
+++ simpleshell.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #ifdef LINUX
@@ -14,14 +15,45 @@ typedef long uint32
 char *readline(void);
 int main(int argc, char *argv[]) {
         char myprompt[] = "vaibshell:~$ ", *line = NULL;
+	char *temp = NULL, *str, *token, *saveptr, *command, **argp;
+	int i, j;
+	char malloc_error[] = "malloc failed:\n";
         while(1) {
                 write(1, "\n", 1);
                 write(1, myprompt, strlen(myprompt));
                 //sleep(2);
                 line = readline();
+		argp = (char **) malloc((((strlen(line) + 1) / 2) + 1)* sizeof(char *));
+		if(argp == NULL) {
+			write(1, malloc_error, sizeof(malloc_error));
+			exit(EXIT_FAILURE);
+		}
                 write(1, line, strlen(line));
                 //Now, check the no. of void spaces in the input excluding it at the first and at the end
+		temp = (char *)malloc(strlen(line) + 1);
+		if(temp == NULL) {
+			write(1, malloc_error, sizeof(malloc_error));
+			exit(EXIT_FAILURE);
+		}
+		strcpy(temp, line);
+		i = 0;
+		for(j = 1, str = temp;   ; j++, str = NULL) {
+			token = strtok_r(str, " ", &saveptr);
+			if(token == NULL) {
+				break;
+			}
+			if(j == 1) {
+				command = token;
+			}
+			else {
+				argp[i++] = token;      //Don't we have to allocate memory for each argp[i] ? or it is allocated already?
+			}
+		}
+		argp[i] = NULL;
 		
+		free(line);
+		free(temp);
+		free(argp);
         }
         return 0;
 }
