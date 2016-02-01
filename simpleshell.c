1fb7c0650fabf45fe8b378b701db3f5c940e0c86
vaibhav <vaibhav.kurhe@gmail.com>
2016-02-01 11:01:16 +0530

    Added code for detecting the no. of pipes present in the input command, then creating (pipes + 1) argp arrays, and then storing individual commands within those arrays.

--- simpleshell.c
+++ simpleshell.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -15,7 +15,8 @@
 
 char *readline(void);
 int main(int argc, char *argv[]) {
-	int argcount, dir;
+	int argcount, dir, pipes, narg, copy_start, tmp_i, tmp_j, k;
+	char **tmp_ptr;
 	char malloc_error[] = "malloc failed:\n";
 	char *cwd, *buf = (char *) malloc(MAX * sizeof(char));
 	int size;
@@ -29,7 +30,7 @@ int main(int argc, char *argv[]) {
 		exit(EXIT_FAILURE);
 	}
         char *line = NULL;
-	char *temp = NULL, *str, *token, *saveptr, *command, **argp, *op_filename, *ip_filename;
+	char *temp = NULL, *str, *token, *saveptr, *command, ***argp, *op_filename, *ip_filename;
 	int i, j, status, exec = 0, n;
 	pid_t pid, w;
 	int fd_out, OUT_REDIR, STDOUT; 
@@ -44,6 +45,7 @@ int main(int argc, char *argv[]) {
 		write(1, malloc_error, sizeof(malloc_error));
 		exit(EXIT_FAILURE);
 	}
+	
 	
 	size = MAX - 1;
         while(1) {
@@ -66,6 +68,7 @@ int main(int argc, char *argv[]) {
 
                 //sleep(2);
                 line = readline();
+		printf("completed reading line \n");
 		
 		/*code to check if line is empty */
 		/* Exceptions handling */
@@ -76,12 +79,38 @@ int main(int argc, char *argv[]) {
 		if(strcmp(line, "exit") == 0) {
 			break;
 		}
-		
-		argp = (char **) malloc((((strlen(line) + 1) / 2) + 1)* sizeof(char *));
-		if(argp == NULL) {
+		printf("skiping the exit\n");
+		/* code for checking the no. of pipes("|") in the input command by the user */
+		i = 0;
+		pipes = 0;
+		printf("starting to count no of pipes\n");
+		while(line[i] != '\0') {
+			printf("line[%d] = %c\n", i, line[i]);
+			if(line[i] == '|') {
+				pipes++;
+				printf("'|' detected: pipes = %d\n", pipes);
+			}
+			i++;
+		}
+		printf("no of pipes detected = %d\n", pipes);
+
+		/* code to malloc that no. of argument array which are one greater than the no of pipes in input command */
+		narg = pipes + 1;
+		argp = (char ***) malloc(narg * sizeof(char **));
+		for(j = 0; j < narg; j++) {
+			argp[j] = (char **) malloc((((strlen(line) + 1) / 2) + 1)* sizeof(char *));
+			if(argp[j] == NULL) {
+				write(1, malloc_error, sizeof(malloc_error));
+				exit(EXIT_FAILURE);
+			}
+		}
+		printf("malloc succeded for %d pipes\n", pipes);
+
+		/*argp[0] = (char **) malloc((((strlen(line) + 1) / 2) + 1)* sizeof(char *));
+		if(argp[0] == NULL) {
 			write(1, malloc_error, sizeof(malloc_error));
 			exit(EXIT_FAILURE);
-		}
+		}*/
 		//write(1, line, strlen(line));
 		temp = (char *)malloc((strlen(line) + 1) * sizeof(char));
 		if(temp == NULL) {
@@ -98,18 +127,19 @@ int main(int argc, char *argv[]) {
 			if(token == NULL) {
 				break;
 			}
-			argp[i++] = token;
+			argp[0][i++] = token;
 			/*if(j == 1) {
 				//command = token;
-				argp[i++] = token;
-				//printf("argument[%d] = %s\n", i - 1, argp[i - 1]);
+				argp[0][i++] = token;
+				//printf("argument[%d] = %s\n", i - 1, argp[0][i - 1]);
 			}
 			else {
-				argp[i++] = token;      //Don't we have to allocate memory for each argp[i] ? or it is allocated already?
-				//printf("argument[%d] =  %s\n", i - 1, argp[i - 1]);
+				argp[0][i++] = token;      //Don't we have to allocate memory for each argp[0][i] ? or it is allocated already?
+				//printf("argument[%d] =  %s\n", i - 1, argp[0][i - 1]);
 			}*/
 		}
-		argp[i] = NULL;
+		argp[0][i] = NULL;
+		printf("tokenizing done successfully\n");
 		n = i;
 		argcount = i;
 		
@@ -118,42 +148,95 @@ int main(int argc, char *argv[]) {
 			continue;	//Possible error: continue can be used for while() loop? right?
 		}
 		/*code to execute "cd <directoryname>" command */
-		if(strcmp(argp[0], "cd") == 0) {
+		if(strcmp(argp[0][0], "cd") == 0) {
 			if(argcount == 1) {
-				argp[1] = "/home/vaibhav";
+				argp[0][1] = "/home/vaibhav";
 			}
 			else if(argcount != 2) {
-				fprintf(stderr, "Usage: %s <directory_path>\n", argp[0]);
+				fprintf(stderr, "Usage: %s <directory_path>\n", argp[0][0]);
 				exit(EXIT_FAILURE);
 			}
-			dir = chdir(argp[1]);
+			dir = chdir(argp[0][1]);
 			if(dir == -1) {
 				perror("chdir:");
 				exit(EXIT_FAILURE);
 			}
 			continue;
 		}
+		printf("Came after cd code\n");
+
+		/* code to implement multiple pipes and all possible combinations of < , > and | */
+		printf("Now running the code for pipes\n");
+		i = 0;
+		j = 0;
+		while(i < narg - 1) {	// "narg - 1" since we want to iterate only upto last argp.
+			printf("outer loop: %d\n", i);
+			j = 0;
+			copy_start = 0;
+			k = 0;
+			while(argp[i][j] != NULL) {
+				printf("inner loop: %d\n", j);
+				if(copy_start == 0 && strcmp(argp[i][j], "|") == 0) {
+					printf("pipe found:\n");
+					copy_start = 1;
+					tmp_i = i;
+					tmp_j = j;
+					//tmp_ptr = &(argp[i][j]);
+					j++;
+					continue;
+				}
+				if(copy_start == 1) {
+					printf("copy_start = 1: so about to copy %s from argp[%d][%d] to argp[%d][%d]\n", argp[i][j], i, j, i + 1, k);
+					//strcpy(argp[i + 1][k], argp[i][j]);
+					argp[i + 1][k] = argp[i][j];
+					k++;
+					printf("copied %s from argp[%d][%d] to argp[%d][%d]\n", argp[i][j], i, j, i + 1, k);
+				}
+				j++;
+			}
+			argp[tmp_i][tmp_j] = NULL;
+			//*tmp_ptr = NULL;
+			argp[i + 1][j] = NULL;
+			i++;
+		}
+		printf("Code for pipes ran successfully\n");
+		/*code to check whether above pipe wala code works or not by printing the contents of different argp[i]'s */
+		printf("Now printing the contents of each argp array:\n");
+		i = 0;
+		j = 0;
+		printf("\n");
+		while(i < narg) {
+			j = 0;
+			printf("arg[%d] :\n", i);
+			while(argp[i][j] != NULL) {
+				printf("%d: %s ", j, argp[i][j]);
+				j++;
+			}
+			printf("\n");
+			i++;
+		}
+		printf("printed the contents successfully & now continuing to the while loop again\n");
+		continue;	/* This continue; must be eliminated after I complete the code for pipe handling */
 		
 		/*code to detect ">" and take appropriate action*/
 		i = 0;
 		//printf("Came upto this!\n");
-		while(argp[i] != NULL) {
+		while(argp[0][i] != NULL) {
 			//printf("Came into while loop!\n");
-			if(strcmp(argp[i++], ">") == 0) {
-				strcpy(op_filename, argp[i]);
-				argp[i - 1] = NULL;
+			if(strcmp(argp[0][i++], ">") == 0) {
+				strcpy(op_filename, argp[0][i]);
+				argp[0][i - 1] = NULL;
 				OUT_REDIR = 1;
 				//printf("OUT_REDIR = %d\n", OUT_REDIR);
 				break;
 			}
 		}
-		
 		/*code to detect "<" and take appropriate action*/
 		i = 0;
-		while(argp[i] != NULL) {
-			if(strcmp(argp[i++], "<") == 0) {
-				strcpy(ip_filename, argp[i]);
-				argp[i - 1] = NULL;
+		while(argp[0][i] != NULL) {
+			if(strcmp(argp[0][i++], "<") == 0) {
+				strcpy(ip_filename, argp[0][i]);
+				argp[0][i - 1] = NULL;
 				IN_REDIR = 1;
 				//printf("IN_REDIR = %d\n", IN_REDIR);
 				break;
@@ -165,8 +248,8 @@ int main(int argc, char *argv[]) {
 		//printf("%s ", command);
 		/*i = 0;
 		while(1) {
-			if(argp[i] != NULL)
-				printf("%s ", argp[i++]);
+			if(argp[0][i] != NULL)
+				printf("%s ", argp[0][i++]);
 			else 
 				break;
 		}
@@ -217,7 +300,7 @@ int main(int argc, char *argv[]) {
 
 			
 			//printf("Child pid is %ld \n", (long) getpid());
-			exec = execvp(*argp, argp);
+			exec = execvp(*argp[0], argp[0]);
 			if(exec == -1) {
 				perror("exec failed");
 				exit(EXIT_FAILURE);
@@ -248,7 +331,10 @@ int main(int argc, char *argv[]) {
 		
 		free(line);
 		free(temp);
-		free(argp);
+		free(argp[0]);
+		for(j = 0; j < narg; j++) {
+			free(argp[j]);
+		}
         }
 	free(ip_filename);
 	free(op_filename);
