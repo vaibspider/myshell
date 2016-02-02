0151ecb3e35b6b6a6dcf30b60ab824a224df55fc
vaibhav <vaibhav.kurhe@gmail.com>
2016-02-02 21:11:44 +0530

    Completed successfully for 1 pipe. And not the only problem of multiple pipes and limitation of pipe size.

--- simpleshell.c
+++ simpleshell.c
@@ -7,15 +7,24 @@
 #include <string.h>
 #include <fcntl.h>
 #include <error.h>
+#include <signal.h>
 
 #define MAX 256
 // Below 2 lines are for the function readline()
 //#define SPACE 0
 //#define ALPHA 1
+/*int thr;
+#define OPENSSL_THREAD_DEFINES
+#include <openssl/opensslconf.h>
+#if defined(OPENSSL_THREADS)
+thr = 1;
+#else
+thr = 0;
+#endif*/
 
 char *readline(void);
 int main(int argc, char *argv[]) {
-	int argcount, dir, pipes, narg, copy_start, tmp_i, tmp_j, k, ret;
+	int argcount, dir, pipes, narg, copy_start, tmp_i, tmp_j, k, ret, sig, p_read, p_write, Size, background = 0;
 	int **pipefd;
 	char **tmp_ptr;
 	char malloc_error[] = "malloc failed:\n";
@@ -46,7 +55,7 @@ int main(int argc, char *argv[]) {
 		write(1, malloc_error, sizeof(malloc_error));
 		exit(EXIT_FAILURE);
 	}
-	
+	//printf("Threads = %d\n", thr);
 	
 	size = MAX - 1;
         while(1) {
@@ -69,7 +78,7 @@ int main(int argc, char *argv[]) {
 
                 //sleep(2);
                 line = readline();
-		printf("completed reading line \n");
+		//printf("completed reading line \n");
 		
 		/*code to check if line is empty */
 		/* Exceptions handling */
@@ -80,20 +89,28 @@ int main(int argc, char *argv[]) {
 		if(strcmp(line, "exit") == 0) {
 			break;
 		}
-		printf("skiping the exit\n");
+		
+		/* */
+		/*sig = sigaction();
+		if(sig == -1) {
+			perror("sigaction failed");
+			exit(EXIT_FAILURE);
+		}*/
+		
+		//printf("skiping the exit\n");
 		/* code for checking the no. of pipes("|") in the input command by the user */
 		i = 0;
 		pipes = 0;
-		printf("starting to count no of pipes\n");
+		//printf("starting to count no of pipes\n");
 		while(line[i] != '\0') {
-			printf("line[%d] = %c\n", i, line[i]);
+			//printf("line[%d] = %c\n", i, line[i]);
 			if(line[i] == '|') {
 				pipes++;
-				printf("'|' detected: pipes = %d\n", pipes);
+				//printf("'|' detected: pipes = %d\n", pipes);
 			}
 			i++;
 		}
-		printf("no of pipes detected = %d\n", pipes);
+		//printf("no of pipes detected = %d\n", pipes);
 		
 		/*code for allocating storage for the file descriptors */
 		if(pipes > 0) {
@@ -104,6 +121,7 @@ int main(int argc, char *argv[]) {
 		}
 
 		/* code to malloc that no. of argument array which are one greater than the no of pipes in input command */
+		Size = ((strlen(line) + 1) / 2) + 1;
 		narg = pipes + 1;
 		argp = (char ***) malloc(narg * sizeof(char **));
 		for(j = 0; j < narg; j++) {
@@ -113,7 +131,12 @@ int main(int argc, char *argv[]) {
 				exit(EXIT_FAILURE);
 			}
 		}
-		printf("malloc succeded for %d pipes\n", pipes);
+		for(i = 0; i < narg; i++) {
+			for(j = 0; j < Size; j++) {
+				argp[i][j] = NULL;
+			}
+		}
+		//printf("malloc succeded for %d pipes\n", pipes);
 
 		/*argp[0] = (char **) malloc((((strlen(line) + 1) / 2) + 1)* sizeof(char *));
 		if(argp[0] == NULL) {
@@ -148,9 +171,14 @@ int main(int argc, char *argv[]) {
 			}*/
 		}
 		argp[0][i] = NULL;
-		printf("tokenizing done successfully\n");
+		//printf("tokenizing done successfully\n");
 		n = i;
 		argcount = i;
+		
+		/* exit */
+		if(strcmp(argp[0][0], "exit") == 0) {
+			break;
+		}
 		
 		/* Exceptions handling */
 		if(argcount == 0) {
@@ -172,21 +200,21 @@ int main(int argc, char *argv[]) {
 			}
 			continue;
 		}
-		printf("Came after cd code\n");
+		//printf("Came after cd code\n");
 
 		/* code to implement multiple pipes and all possible combinations of < , > and | */
-		printf("Now running the code for pipes\n");
+		//printf("Now running the code for pipes\n");
 		i = 0;
 		j = 0;
 		while(i < narg - 1) {	// "narg - 1" since we want to iterate only upto last argp.
-			printf("outer loop: %d\n", i);
+			//printf("outer loop: %d\n", i);
 			j = 0;
 			copy_start = 0;
 			k = 0;
 			while(argp[i][j] != NULL) {
-				printf("inner loop: %d\n", j);
+				//printf("inner loop: %d\n", j);
 				if(copy_start == 0 && strcmp(argp[i][j], "|") == 0) {
-					printf("pipe found:\n");
+					//printf("pipe found:\n");
 					copy_start = 1;
 					tmp_i = i;
 					tmp_j = j;
@@ -195,11 +223,11 @@ int main(int argc, char *argv[]) {
 					continue;
 				}
 				if(copy_start == 1) {
-					printf("copy_start = 1: so about to copy %s from argp[%d][%d] to argp[%d][%d]\n", argp[i][j], i, j, i + 1, k);
+					//printf("copy_start = 1: so about to copy %s from argp[%d][%d] to argp[%d][%d]\n", argp[i][j], i, j, i + 1, k);
 					//strcpy(argp[i + 1][k], argp[i][j]);
 					argp[i + 1][k] = argp[i][j];
 					k++;
-					printf("copied %s from argp[%d][%d] to argp[%d][%d]\n", argp[i][j], i, j, i + 1, k);
+					//printf("copied %s from argp[%d][%d] to argp[%d][%d]\n", argp[i][j], i, j, i + 1, k);
 				}
 				j++;
 			}
@@ -208,23 +236,23 @@ int main(int argc, char *argv[]) {
 			argp[i + 1][j] = NULL;
 			i++;
 		}
-		printf("Code for pipes ran successfully\n");
+		//printf("Code for pipes ran successfully\n");
 		/*code to check whether above pipe wala code works or not by printing the contents of different argp[i]'s */
-		printf("Now printing the contents of each argp array:\n");
+		//printf("Now printing the contents of each argp array:\n");
 		i = 0;
 		j = 0;
-		printf("\n");
+		//printf("\n");
 		while(i < narg) {
 			j = 0;
-			printf("arg[%d] :\n", i);
+			//printf("arg[%d] : ", i);
 			while(argp[i][j] != NULL) {
-				printf("%d: %s ", j, argp[i][j]);
+				//printf("%s ", argp[i][j]);
 				j++;
 			}
-			printf("\n");
+			//printf("\n");
 			i++;
 		}
-		printf("printed the contents successfully & now continuing to the while loop again\n");
+		//printf("printed the contents successfully & now continuing to the while loop again\n");
 		//continue;	/* This continue; must be eliminated after I complete the code for pipe handling */
 		
 		/*code to detect ">" and take appropriate action*/
@@ -263,18 +291,100 @@ int main(int argc, char *argv[]) {
 				break;
 		}
 		printf("\n\n");*/
-		printf("Now starting the forking\n");
-		for(i = 0; i < narg; i++) {
+		//printf("Now starting the forking\n");
+		//printf("narg = %d\n", narg);
+		
+		if(pipes > 0) {
+			for(i = 0; i < pipes; i++) {
+				ret = pipe(pipefd[i]);
+				if(ret == -1) {
+					perror("pipe failed:");	
+					exit(EXIT_FAILURE);
+				}
+			}
+		}
+		p_read = 0;
+		p_write = 0;
+		for(i = 0; i < narg; i++, p_read++, p_write++) {
+			if(i == 1) {
+				p_read--;
+			}
+			//printf("p_read = %d  p_write = %d\n", p_read, p_write);
 			pid = fork();
-			printf("fork %d done\n", i);
+			//printf("fork %d done\n", i);
 			if(pid == -1) {
 				perror("fork");
 				exit(EXIT_FAILURE);
 			}
 			if(pid == 0) {// that means it is child
-				printf("In child now:\n");
+				//printf("In child now:\n");
+
+				/* code for Output and Input Redirection */
+				/* code for identifying Input and Output Filenames */
+				j = 0;
+				while(argp[i][j] != NULL) {
+					if(strcmp(argp[i][j], "&") == 0) {
+						background = 1;
+					}
+					if(strcmp(argp[i][j], ">") == 0) {
+						OUT_REDIR = 1;
+						if(j == 0) {
+							fprintf(stdout, "Usage: \"command < input > output\"\n or \"command > output < input\"\n"); 
+						}
+						strcpy(op_filename, argp[i][j + 1]);
+					}
+					else if(strcmp(argp[i][j], "<") == 0) {
+						IN_REDIR = 1;
+						if(j == 0) {
+							fprintf(stdout, "Usage: \"command < input > output\"\n or \"command > output < input\"\n"); 
+						}
+						strcpy(ip_filename, argp[i][j + 1]);
+					}
+					j++;
+				}
+				
+				/* code for putting NULL in the argp[i] in the place of first of "<" and ">"*/
+				if(OUT_REDIR == 1 || IN_REDIR == 1) {
+					//printf("out_redir = %d and in_redir = %d\n", OUT_REDIR, IN_REDIR);
+					j = 0;
+					while(argp[i][j] != NULL) {
+						if(strcmp(argp[i][j], ">") == 0 || strcmp(argp[i][j], "<") == 0) {
+							argp[i][j] = NULL;
+							//tmp_i = i;
+							//tmp_j = j;
+							break;
+						}
+						j++;
+					}
+					//argp[tmp_i][tmp_j] = NULL;
+				}
+				if(background == 1) {
+					j = 0;
+					while(argp[i][j] != NULL) {
+						if(strcmp(argp[i][j], "&") == 0) {
+							argp[i][j] = NULL;
+							break;
+						}
+						j++;
+					}
+				}
+				
+				/* code to print the contents of argp[i] */
+				k = 0;
+				j = 0;
+				/*printf("\n");
+				while(k < narg) {
+					j = 0;
+					printf("arg[%d] : ", k);
+					while(argp[k][j] != NULL) {
+						printf("%s ", argp[k][j]);
+						j++;
+					}
+					printf("\n");
+					k++;
+				}*/
 				/* Code for output redirection */
-				/*if(OUT_REDIR) {
+				if(OUT_REDIR) {
 					STDOUT = dup(1);
 					close(1);
 					fd_out = open(op_filename, O_WRONLY | O_CREAT, S_IRWXU);
@@ -284,10 +394,10 @@ int main(int argc, char *argv[]) {
 					}
 					//printf("fd_out = %d\n", fd_out);
 					
-				}*/
+				}
 
 				/* Code for input redirection */
-				/*if(IN_REDIR) {
+				if(IN_REDIR) {
 					STDIN = dup(0);
 					close(0);
 					fd_in = open(ip_filename, O_RDONLY);
@@ -297,48 +407,85 @@ int main(int argc, char *argv[]) {
 					}
 					//printf("fd_in = %d\n", fd_in);
 					
-				}*/
+				}
 				
-				/* Code for pipe() */
 
-				/*if(PIPE) {		// Assuming that only pipe is present. Mix of > , < and | will be done later.
-					if(pipe(pipefd) == -1) {
-						perror("pipe failed");
-						exit(EXIT_FAILURE);
-					}
-					
-				}*/
-
+				//printf("i = %d\n", i);
 				if(narg > 1) {
-					printf("narg > 1\n");
-					ret = pipe(pipefd[i]);
-					if(ret == -1) {
-						perror("pipe failed");	
-						exit(EXIT_FAILURE);
-					}
 					if(i == 0) {
-						close(pipefd[i][0]);
-						STDOUT = dup(1);
-						close(1);
-						if(dup2(pipefd[i][1], 1) == -1) {
-							write(STDOUT, "dup2 error\n", 12);
-							exit(EXIT_FAILURE);
+						close(pipefd[p_write][0]);
+						if(OUT_REDIR == 0) {
+							STDOUT = dup(STDOUT_FILENO);
+							if(STDOUT == -1) {
+								perror("dup stdout failed");
+								exit(EXIT_FAILURE);
+							}
+							//close(1);
+							if(dup2(pipefd[p_write][1], STDOUT_FILENO) == -1) {
+								write(STDOUT, "dup2 error\n", 12);
+								exit(EXIT_FAILURE);
+							}
+							//write(STDOUT, "pipefd out succeded\n", 21);
 						}
+						
 					}
 					else if(i == narg - 1) {
-						close(pipefd[i][1]);
-						STDIN = dup(0);
-						close(1);
-						if(dup2(pipefd[i][1], 0) == -1) {
-							write(STDIN, "dup2 error\n", 12);
+						//printf("came in stdin change:\n");
+						close(pipefd[p_read][1]);
+						//printf("pipefd[0][1] done\n");
+						/*if(dup2(0, STDIN) == -1) {
+							printf("error pipefd in\n");
+							//write(1, "dup2 error: 1\n", 14);
+							perror("dup2 error:");
 							exit(EXIT_FAILURE);
+						}*/
+						/*STDIN = dup(STDIN_FILENO);
+						if(STDIN == -1) {
+							perror("dup stdin failed");
+							exit(EXIT_FAILURE);
+						}*/
+						//close(1);
+						//close(0);
+						//printf("no error pipefd IN :\n");
+						if(IN_REDIR == 0) {
+							if(dup2(pipefd[p_read][0], STDIN_FILENO) == -1) {
+								//printf("error\n");
+								//write(STDOUT, "dup2 error\n", 12);
+								perror("dup2 IN error:");
+								exit(EXIT_FAILURE);
+							}
+							//printf("came finally:\n");
+							//write(1, "pipefd in succeded\n", 20);
 						}
 					}
 					else {
 						
+						//printf("now in else pipefd:\n");
+						close(pipefd[p_read][1]);
+						close(pipefd[p_write][0]);
+						if(IN_REDIR == 0) {
+							if(dup2(pipefd[p_read][0], STDIN_FILENO) == -1) {
+								perror("pipefd IN failed:\n");
+								exit(EXIT_FAILURE);
+							}
+							//printf("3 complete\n");
+						}
+						if(OUT_REDIR == 0) {
+							STDOUT = dup(STDOUT_FILENO);
+							if(STDOUT == -1) {
+								perror("dup stdout failed");
+								exit(EXIT_FAILURE);
+							}
+							if(dup2(pipefd[p_write][1], STDOUT_FILENO) == -1) {
+								write(STDOUT, "pipefd else failed:\n", 20);
+								//perror("pipefd OUT failed:\n");
+								exit(EXIT_FAILURE);
+							}
+							//write(STDOUT, "pipefd else succeded\n", 21);
+						}
 					}
 				}
-				printf("now doing execvp\n");
+				//printf("now doing execvp\n");
 				//printf("Child pid is %ld \n", (long) getpid());
 				exec = execvp(*argp[i], argp[i]);
 				if(exec == -1) {
@@ -347,26 +494,42 @@ int main(int argc, char *argv[]) {
 				}
 			}
 			else {		//that means it is parent
+				//for(j = 0; j < pipes; j++) {
+				//	wait(NULL);
+				//}
+					
 				// I didn't understand why we need to use loop, why can't we use waitpid() just once?
-				do {
-					w = waitpid(pid, &status, WUNTRACED | WCONTINUED);
-					if(w == -1) {
-						perror("waitpid");
-						exit(EXIT_FAILURE);
+				//if(i == narg - 1) {
+				if(background == 0) {
+					do {
+						//printf("parent waiting...\n");
+						w = waitpid(pid, &status, WUNTRACED | WCONTINUED);
+						if(w == -1) {
+							perror("waitpid");
+							exit(EXIT_FAILURE);
+						}
+						if(WIFEXITED(status)) {
+							printf("exited, status=%d\n", WEXITSTATUS(status));
+						}
+						else if(WIFSIGNALED(status)) {
+							printf("killed by signal %d\n", WTERMSIG(status));
+						}
+						else if(WIFSTOPPED(status)) {
+							printf("stopped by signal %d\n", WSTOPSIG(status));
+						}
+						else if(WIFCONTINUED(status)) {
+							printf("continued\n");
+						}
+					} while(!WIFEXITED(status) && !WIFSIGNALED(status));
+					if(pipes > 0 && i == 0) {
+						close(pipefd[p_write][1]);
 					}
-					if(WIFEXITED(status)) {
-						//printf("exited, status=%d\n", WEXITSTATUS(status));
-					}
-					else if(WIFSIGNALED(status)) {
-						//printf("killed by signal %d\n", WTERMSIG(status));
-					}
-					else if(WIFSTOPPED(status)) {
-						//printf("stopped by signal %d\n", WSTOPSIG(status));
-					}
-					else if(WIFCONTINUED(status)) {
-						//printf("continued\n");
-					}
-				} while(!WIFEXITED(status) && !WIFSIGNALED(status));
+				}
+				
+				/*close(pipefd[p_write][1]);
+				if(i == narg-1)
+					close(*/
+				//}
 			}
 		}
 		
@@ -382,6 +545,8 @@ int main(int argc, char *argv[]) {
 			}
 			free(pipefd);
 		}
+		close(fd_in);
+		close(fd_out);
         }
 	free(ip_filename);
 	free(op_filename);
